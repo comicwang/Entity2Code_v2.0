485aa0527d2259a7be37fd721bd308d1fa62d24c
comicwang <672802899@qq.com>
2017-06-14 01:03:37 +0800

    Base

---
+++ Utility/ConfirmResource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Utility
+{
+    public class ConfirmResource
+    {
+        public static void Copy()
+        {
+            string ini = Properties.Resource.CodeIni;
+            string iniPath = Path.Combine(ConstCommon.RootPath, "Code.ini");
+            if (!File.Exists(iniPath))
+                FileOprateHelp.SaveTextFile(ini, iniPath);
+
+            Bitmap img = Properties.Resource.img;
+            string imgPath = Path.Combine(ConstCommon.RootPath, "img.jpg");
+            if (!File.Exists(imgPath))
+                img.Save(imgPath, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+
+        }
+    }
+}
